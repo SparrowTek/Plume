50a40e429d03afb0d2594be0f53dc54677456aa2
Thomas Rademaker <thomas@radmakr.com>
2025-03-24 15:30:38 -0400

    initial commit

---
+++ plume/System/PlumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  plumeApp.swift
+//  plume
+//
+//  Created by Thomas Rademaker on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct PlumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
